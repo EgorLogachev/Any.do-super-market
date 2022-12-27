1d67ecedc29f61e3ec29493717ae9e2f4549b53b
Yegor Logachev <egor@itomy.ch>
2022-12-27 14:12:32 +0200

    UI has been added to the test proj

---
+++ app/src/main/java/com/example/superdomarket/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.superdomarket
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.superdomarket.ui.theme.SuperdoMarketTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperdoMarketTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ProductsList()
+                }
+            }
+        }
+    }
+}
+
+//{"bagColor": "#64BD4F", "name": "Onion", "weight": "1.8kg"}	1672139796.9766734
+
+@Composable
+private fun ProductsList(products: List<Product>) {
+
+    LazyColumn {
+        items(products) { item: Product ->
+            ProductItem(item)
+        }
+    }
+}
+
+@Composable
+private fun ProductItem(
+    product: Product,
+    onIconClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 8f.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(color = product.color)
+                .border(1.dp, Color(red = 136, green = 136, blue = 136), shape = CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    1.dp,
+                    Color(red = 221, green = 221, blue = 221),
+                    shape = RoundedCornerShape(3.dp)
+                )
+        ) {
+            Surface(
+                modifier = Modifier.fillMaxWidth(),
+                color = Color(red = 245, green = 245, blue = 245)
+            ) {
+                Text(text = product.name, modifier = Modifier.padding(all = 8.dp))
+            }
+            Spacer(
+                modifier = Modifier
+                    .height(1.dp)
+                    .background(color = Color(red = 221, green = 221, blue = 221))
+            )
+            Text(text = product.weight, modifier = Modifier.padding(all = 8.dp))
+        }
+    }
+
+
+}
+
+data class Product(val color: Color, val name: String, val weight: String)
+
+fun Color.fromHex(color: String) = Color(android.graphics.Color.parseColor(color))
